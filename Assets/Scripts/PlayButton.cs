af2133f35dab26c952a003f60f71b2b2bb61e1f6
Henry Chambers <chambers6h@gmail.com>
2026-02-25 23:45:15 -0700

    fixed mouse issues

--- Assets/Scripts/PlayButton.cs
+++ Assets/Scripts/PlayButton.cs
@@ -3,6 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class PlayButton : MonoBehaviour
 {
+    private void Awake()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
